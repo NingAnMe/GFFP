2405efc6db947ba34c97855af06e3b65a1a208d9
NingAnMe <ninganme@qq.com>
2020-11-24 17:47:53 +0800

    增加结果目录

--- user_config.py
+++ user_config.py
@@ -4,6 +4,8 @@
 # @Author  : NingAnMe <ninganme@qq.com>
 import os
 
-# DATA_ROOT = '/DISK/DATA02/PROJECT/SourceData/GongFu/GFFP/DATA/'  # 数据根目录
 DATA_ROOT = './DATA/'  # 数据根目录
 DATA_1KM = os.path.join(DATA_ROOT, '1KM')  # 插值后的1KM数据
+DATA_PICTURE = os.path.join(DATA_ROOT, 'PICTURE')  # 图片结果
+DATA_STAT = os.path.join(DATA_ROOT, 'STAT')  # 统计结果
+DATA_DOC = os.path.join(DATA_ROOT, 'DOC')  # 报告结果
