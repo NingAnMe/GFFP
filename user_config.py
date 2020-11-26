804c922fe0f818ffaa96b69c17bbe97497741bc8
NingAnMe <ninganme@qq.com>
2020-11-26 18:15:49 +0800

    路径改为绝对路径

--- user_config.py
+++ user_config.py
@@ -4,7 +4,8 @@
 # @Author  : NingAnMe <ninganme@qq.com>
 import os
 
-DATA_ROOT = './DATA/'  # 数据根目录
+DATA_ROOT = '/home/kts_project_v1/gffp/DATA/'  # 数据根目录
+# DATA_ROOT = './DATA/'  # 数据根目录
 DATA_1KM = os.path.join(DATA_ROOT, '1KM')  # 插值后的1KM数据
 DATA_PICTURE = os.path.join(DATA_ROOT, 'PICTURE')  # 图片结果
 DATA_STAT = os.path.join(DATA_ROOT, 'STAT')  # 统计结果
